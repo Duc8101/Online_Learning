567346a8c17ef53c61df2e234933f541354d4789
Duc8101 <vegitobasicinstict1810@gmail.com>
2026-06-01 16:20:44 +0700

    commit query profile service

--- src/main/java/com/onlinelearning/repository/UserRepository.java
+++ src/main/java/com/onlinelearning/repository/UserRepository.java
@@ -3,11 +3,14 @@ package com.onlinelearning.repository;
 import com.onlinelearning.model.dto.response.UserForHomePageResponseDto;
 import com.onlinelearning.model.dto.response.UserProfileResponseDto;
 import com.onlinelearning.model.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
@@ -34,4 +37,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
             + "SELECT 1 from User u where u.userAccount.username = :username or u.email = :email"
             + ")")
     boolean isUsernameOrEmailExist(String username, String email);
+
+    @Query("select exists (\n"
+            + "SELECT 1 from User u where u.email = :email and u.userId <> :userId\n"
+            + ")")
+    boolean isEmailExist(String email, long userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.fullName = :fullName, u.phone = :phone, u.email = :email, u.address = :address, u.gender = :gender, u.image = :image, u.updatedAt = :updatedAt where u.userId = :userId")
+    void updateProfile(String fullName, String phone, String email, String address, String gender, String image, Instant updatedAt, long userId);
 }
